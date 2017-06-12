a43043a59f165c678afec8eb8bfe7e0809268cda
KarlaNavaM <karla.nm8@gmail.com>
2017-06-12 11:10:51 -0500

    Creamos espacio para poder ver detalles de planeta, ingresamos mediante JSON a características de otra url

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,16 @@
 var cargarPagina = function() {
     cargarPersonajes();
+    $(document).on("click", ".personaje", mostrarDetallePersonaje)
 };
 
 var cargarPersonajes = function() {
-  $.ajax('http://swapi.co/api/people/', {
-      method: 'GET',
-      dataType: 'json',
-      success: function(response) {
-          var personajes = response.results;
-          var total = response.count;
-          mostrarTotalPersonajes(total);
-          mostrarPersonajes(personajes);
-      },
-      error: function(error) {
-          console.log("error", error);
-      }
-  });  
+    var url ='http://swapi.co/api/people';
+    $.getJSON(url, function(response){
+        var personajes = response.results;
+        var total = response.count;
+        mostrarTotalPersonajes(total);
+        mostrarPersonajes(personajes);
+    });
 };
 
 var mostrarTotalPersonajes = function(total) {
@@ -29,10 +24,28 @@ var mostrarPersonajes = function(personajes) {
               var $ul = $('#personajes');
               
           $ul.append($li);
+         $li.attr("data-url", personaje.homeworld);
+         $li.addClass("personaje");
           $li.text(personaje.name);
               console.log(personaje.name);
           }); 
     
+};
+
+var plantilla = '<h2>Planeta</h2>' +
+      '<p><strong>Nombre: </strong>__nombre__</p>' +
+      '<p><strong>Clima: </strong>__clima__</p>';
+
+
+var mostrarDetallePersonaje = function(total) {
+    var url = $(this).data('url');
+    var $planetaContenedor = $('#DetallePlaneta');
+    $.getJSON(url, function(response) {
+        $planetaContenedor.html(
+        plantilla.replace('__nombre__', response.name)
+                .replace('__clima__', response.climate)
+            );
+    });
 };
 
 
